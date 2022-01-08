904ad0359498f185fb6b733f73d707dde8b3c6ae
WhyIsCronTaken <47146925+WhyIsCronTaken@users.noreply.github.com>
2022-01-08 19:56:24 +0000

    Update romlist.js

--- romlist.js
+++ romlist.js
@@ -1,4 +1,4 @@
 var ROMLIST = [
     {url:"roms/Paper Mario (USA).z64",title:"Paper Mario"},
-    {url:"roms/Paper Mario Black Pit (USA).z64",title:"Paper Mario: Black Pit"},
+    {url:"roms/Paper Mario Black Pit (USA).z64",title:"Paper Mario; Black Pit"},
 ];
